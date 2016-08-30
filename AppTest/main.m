999617236e26f05fa0a07f5f011a67db6b18403e
Rafael Perez <rafaelperez@rafaels-macbook-pro.local>
2016-08-29 22:27:56 -0400

    Initial Commit

---
+++ AppTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppTest
+//
+//  Created by Rafael Perez on 8/29/16.
+//  Copyright © 2016 Rafael Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
